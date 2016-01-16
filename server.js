d80407bd6b19924a7b60240b0c1d01ad40bbfd06
sinhvis <sinhvis@gmail.com>
2016-01-15 21:19:33 -0800

    Routes for single page app

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 // get the things we need
-var express	= require('express') ;
-var app		= express() ;
-var path	= require('path') ;
+var express = require('express');
+var app     = express();
+var path = require('path');
 
-// set up the public folder to server public assets
-app.use(express.static(__dirname + '/public')) ;
+// set the public folder to serve public assets
+app.use(express.static(__dirname + '/public'));
 
-// set up one route to the index.html file
+// set up our one route to the index.html file
 app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/views/index.html')) ;
-}) ;
+	res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
 
 // start the server on port 8080 (http://localhost:8080)
-app.listen(8080) ;
-console.log('Magic happens on port 8080') ;
+app.listen(8080);
+console.log('Magic happens on port 8080.');
